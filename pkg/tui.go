78422ffdd0c881d6565d27c07d2e04767cbad70e
Josh Allan <joshua.allan92@gmail.com>
2026-04-22 08:34:16 +1000

    chore(deps): add bubbletea and bubbles dependencies

---
+++ pkg/tui.go
@@ -0,0 +1,9 @@
+package ahab
+
+import (
+	_ "github.com/charmbracelet/bubbles"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+// Placeholder for TUI implementation (Task 1 of 8)
+var _ = tea.Program{}
